5674fb81db4deb32161d1411040839aabcc1e9c5
Erin-sora-oh <mmelse76@gmail.com>
2020-02-12 17:20:43 +0900

    Add UserController [insert]

--- UserController.java
+++ UserController.java
@@ -2,10 +2,17 @@
 public class UserController{
 
 	//로그인 페이지 이동
-	@RequestMapping("user/login/do")
+	@RequestMapping("user/login.do")
 	public String login(){
 		logger.info("경로  : login");
 		return "user/login";
+		
+	}
+
+	@RequestMapping("user/insert.do")
+	public String insert(){
+		logger.info("경로  : insert");
+		return "user/insert";
 		
 	}
 }
